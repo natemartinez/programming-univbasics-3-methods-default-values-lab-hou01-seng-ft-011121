43f97d6606422a8698e8f70e1ed03947411fd858
Nathaniel Martinez <nathanielmartinez2001@gmail.com>
2020-12-19 19:30:35 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,5 +1,8 @@
 def meal_choice(veg1,veg2,protein=tofu)
-  puts "What a nutritious meal! A plate of #{protein} with #{veg1} and #{veg2}."
+  puts "What a nutritious meal!"
+  meal="A plate of #{protein} with #{veg1} and #{veg2}."
+  puts meal
+  meal
   
-  meal_choice('carrots','string beans','lentils')
+  meal_choice('carrots','string beans')
   
